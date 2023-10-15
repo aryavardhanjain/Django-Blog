73544afb1984ea8b36e403be4280ba3ae5da4be6
Aryavardhan Jain <jainaryan418@gmail.com>
2023-10-15 21:34:28 +0530

    blog model setup

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'blog_main/static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT  = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
