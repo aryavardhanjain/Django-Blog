bd225caa1b0001e81c8aab4fd91aa286f2403205
Aryavardhan Jain <jainaryan418@gmail.com>
2023-10-22 22:22:35 +0530

    Comment Feature

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.http.request import HttpRequest
-from .models import Category, Blog, About, SocialLink
+from .models import Category, Blog, About, SocialLink, Comment
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('title', )}
@@ -21,3 +21,4 @@ admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(About, AboutAdmin)
 admin.site.register(SocialLink)
+admin.site.register(Comment)
